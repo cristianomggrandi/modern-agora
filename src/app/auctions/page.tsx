5644c156cdf60ce807f26a004f7c46e27f3d5f93
Cristiano Grandi <80713582+cristianomggrandi@users.noreply.github.com>
2024-05-24 12:59:57 -0300

    Transform layout into grid when on mobile

--- src/app/auctions/page.tsx
+++ src/app/auctions/page.tsx
@@ -26,16 +26,16 @@ const AuctionCountdown = ({ auction }: { auction: NDKAuctionContent }) => {
     const significantTimer = days !== 0 ? days : hours !== 0 ? hours : minutes !== 0 ? minutes : seconds
 
     return (
-        <div className="grid md:grid-cols-4 gap-x-4 text-lg font-bold *:w-12 text-center *:m-auto *:neon-text-sm">
-            <span className="hidden md:block">D</span>
-            <span className="hidden md:block">H</span>
-            <span className="hidden md:block">M</span>
-            <span className="hidden md:block">S</span>
+        <div className="hidden sm:grid md:grid-cols-4 gap-x-4 text-lg font-bold *:w-12 text-center *:m-auto">
+            <span className="hidden md:block neon-text-sm">D</span>
+            <span className="hidden md:block neon-text-sm">H</span>
+            <span className="hidden md:block neon-text-sm">M</span>
+            <span className="hidden md:block neon-text-sm">S</span>
             <span className="md:hidden">{significantTimerName}</span>
-            <span className="hidden md:block">{Math.floor(days)}</span>
-            <span className="hidden md:block">{Math.floor(hours)}</span>
-            <span className="hidden md:block">{Math.floor(minutes)} </span>
-            <span className="hidden md:block">{Math.floor(seconds)}</span>
+            <span className="hidden md:block neon-text-sm">{Math.floor(days)}</span>
+            <span className="hidden md:block neon-text-sm">{Math.floor(hours)}</span>
+            <span className="hidden md:block neon-text-sm">{Math.floor(minutes)} </span>
+            <span className="hidden md:block neon-text-sm">{Math.floor(seconds)}</span>
             <span className="md:hidden">{Math.floor(significantTimer)}</span>
         </div>
     )
@@ -44,27 +44,31 @@ const AuctionCountdown = ({ auction }: { auction: NDKAuctionContent }) => {
 const AuctionCard = ({ event, highestBid }: { event: NDKParsedAuctionEvent; highestBid?: number }) => {
     if (!event.content) return null
 
-    // TODO: Change layout to grid instead of list when on mobile
+    // TODO: Avaliate: When on mobile, make img the background so the text is over it
 
     return (
-        <Link className="flex justify-center divide-x divide-nostr divide-spa *:px-1 md:*:px-3" href={"/auction/" + event.content.id}>
-            <div className="h-24 w-24 flex-shrink-0 flex items-center p-2">
+        <Link
+            className="flex flex-col grow gap-2 sm:gap-0 p-1 sm:p-0 sm:flex-row w-40 sm:w-auto justify-center sm:divide-x divide-nostr border sm:border-x-0 sm:first:border-none border-nostr rounded-lg sm:rounded-none *:px-1 md:*:px-3"
+            href={"/auction/" + event.content.id}
+        >
+            <div className="h-24 w-full sm:w-24 flex-shrink-0 flex items-center justify-center sm:p-2">
                 {event.content.images ? (
                     <img
-                        className="w-full max-w-20 max-h-20 rounded"
+                        className="h-24 max-w-full sm:max-w-20 sm:max-h-20 rounded"
                         src={event.content.images[0]}
                         alt={event.content.name}
-                        width={48}
-                        height={48}
+                        height={96}
+                        // width={96}
                     />
                 ) : null}
             </div>
-            <div className="flex-1 flex flex-col justify-center">
-                <span className="font-semibold">{event.content.name}</span>
-                <span className="line-clamp-2 text-sm">{event.content.description}</span>
+            <div className="flex-1 flex flex-col justify-between sm:justify-around">
+                <span className="line-clamp-2 font-semibold">{event.content.name}</span>
+                <span className="line-clamp-3 sm:line-clamp-2 text-sm">{event.content.description}</span>
             </div>
-            <div className="flex flex-col items-center justify-around sm:w-16">
+            <div className="flex sm:flex-col items-center sm:justify-around sm:w-16">
                 <span className="font-bold uppercase neon-text-sm">Bid</span>
+                <span className="font-bold uppercase neon-text-sm sm:hidden mr-1">:</span>
                 {/* TODO: Add currency */}
                 <span className="font-bold uppercase neon-text-sm">{nFormatter(highestBid ?? event.content.starting_bid, 2)}</span>
             </div>
@@ -78,11 +82,11 @@ export default function Auctions() {
     const bids = useBids()
 
     return (
-        <main className="flex items-center justify-center p-16">
-            <div className="divide-y divide-nostr border border-nostr shadow-nostr shadow-sm rounded-lg">
+        <main className="flex items-center justify-center p-8 sm:p-16">
+            <div className="flex flex-wrap gap-4 sm:gap-0 sm:block sm:divide-y divide-nostr sm:border border-nostr shadow-nostr sm:shadow-sm rounded-lg">
                 {/* TODO: Handle auction limiting better, maybe paginate */}
-                {auctions.slice(0, 20).map((auctionEvent, index) => (
-                    <AuctionCard key={auctionEvent.id + index} event={auctionEvent} highestBid={bids.get(auctionEvent.id)} />
+                {auctions.slice(0, 10).map((event, index) => (
+                    <AuctionCard key={event.id + index} event={event} highestBid={bids.get(event.id)} />
                 ))}
             </div>
         </main>
