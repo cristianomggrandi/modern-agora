d524acda6ba70fbb6bf6996ee76bbc6a9046ed27
Cristiano Grandi <80713582+cristianomggrandi@users.noreply.github.com>
2024-07-06 16:03:30 -0300

    Simplify product design with glasslike background effect

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -12,13 +12,17 @@ const ProductCard = ({ event }: { event: NDKParsedProductEvent }) => {
 
     return (
         <Link
-            className="flex flex-col gap-2 md:gap-0 p-1 md:p-0 md:flex-row md:w-auto justify-center md:divide-x divide-nostr border md:border-x-0 md:first:border-none border-nostr rounded-lg md:rounded-none *:px-1 md:*:px-3"
+            className="relative flex flex-col gap-2 p-1 justify-center hover:outline outline-nostr rounded-lg"
             href={"/product/" + event.content.id}
         >
-            <div className="h-24 w-full md:w-24 flex-shrink-0 flex items-center justify-center md:p-2">
+            <div className="relative aspect-square w-full flex-shrink-0 flex items-center justify-center overflow-hidden">
+                <div
+                    className="absolute w-full h-full blur-sm bg-center bg-cover bg-no-repeat"
+                    style={{ backgroundImage: event.content.images ? `url(${event.content.images[0]})` : undefined }}
+                />
                 {event.content.images?.length ? (
                     <img
-                        className="max-h-24 max-w-full md:max-w-20 md:max-h-20 rounded"
+                        className="z-10 max-h-full max-w-full rounded"
                         src={event.content.images[0]}
                         alt={event.content.name}
                         height={96}
@@ -26,20 +30,45 @@ const ProductCard = ({ event }: { event: NDKParsedProductEvent }) => {
                     />
                 ) : null}
             </div>
-            <div className="flex-1 flex flex-col justify-between md:justify-around">
-                <span className="line-clamp-2 font-semibold">{event.content.name}</span>
-                <span className="line-clamp-3 md:line-clamp-2 text-sm">{event.content.description}</span>
-            </div>
-            <div className="flex md:flex-col justify-end items-center md:justify-around md:w-28">
-                <span className="font-bold uppercase neon-text-sm">
+            <div className="flex-1 flex flex-col justify-between">
+                <span className="line-clamp-2 text-sm font-semibold">{event.content.name}</span>
+                <span className="text-sm font-bold uppercase neon-text-sm text-right">
                     {nFormatter(event.content.price, 2)} {event.content.currency}
                 </span>
             </div>
         </Link>
     )
+
+    // return (
+    //     <Link
+    //         className="flex flex-col gap-2 md:gap-0 p-1 md:p-0 md:flex-row md:w-auto justify-center md:divide-x divide-nostr border md:border-x-0 md:first:border-none border-nostr rounded-lg md:rounded-none *:px-1 md:*:px-3"
+    //         href={"/product/" + event.content.id}
+    //     >
+    //         <div className="h-24 w-full md:w-24 flex-shrink-0 flex items-center justify-center md:p-2">
+    //             {event.content.images?.length ? (
+    //                 <img
+    //                     className="max-h-24 max-w-full md:max-w-20 md:max-h-20 rounded"
+    //                     src={event.content.images[0]}
+    //                     alt={event.content.name}
+    //                     height={96}
+    //                     // width={96}
+    //                 />
+    //             ) : null}
+    //         </div>
+    //         <div className="flex-1 flex flex-col justify-between md:justify-around">
+    //             <span className="line-clamp-2 font-semibold">{event.content.name}</span>
+    //             <span className="line-clamp-3 md:line-clamp-2 text-sm">{event.content.description}</span>
+    //         </div>
+    //         <div className="flex md:flex-col justify-end items-center md:justify-around md:w-28">
+    //             <span className="font-bold uppercase neon-text-sm">
+    //                 {nFormatter(event.content.price, 2)} {event.content.currency}
+    //             </span>
+    //         </div>
+    //     </Link>
+    // )
 }
 
-const ITEMS_PER_PAGE = 5
+const ITEMS_PER_PAGE = 12
 
 export default function Products() {
     const [products, setProducts] = useState<NDKParsedProductEvent[]>([])
@@ -74,7 +103,8 @@ export default function Products() {
 
     return (
         <main className="flex flex-col items-center justify-stretch p-4">
-            <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(10rem,1fr))] gap-4 md:block md:divide-y divide-nostr md:border border-nostr shadow-nostr md:shadow-sm rounded-lg">
+            <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(10rem,1fr))] gap-6 rounded-lg">
+                {/* <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(10rem,1fr))] gap-4 md:block md:divide-y divide-nostr md:border border-nostr shadow-nostr md:shadow-sm rounded-lg"> */}
                 {products.slice(ITEMS_PER_PAGE * (page - 1), ITEMS_PER_PAGE * page).map(event => (
                     <ProductCard key={event.id} event={event} />
                 ))}
