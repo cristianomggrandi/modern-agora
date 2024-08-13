4d9433ebee930f9f868db9e92c45beb66509576d
Cristiano Grandi <80713582+cristianomggrandi@users.noreply.github.com>
2024-08-12 23:13:16 -0300

    Place tags below images

--- src/app/components/ProductTags.tsx
+++ src/app/components/ProductTags.tsx
@@ -1,15 +1,14 @@
 import { NDKTag } from "@nostr-dev-kit/ndk"
 
 function EventTag(props: { tag: NDKTag }) {
-    return <span className="text-nowrap bg-nostr text-white rounded-xl px-2 py-1 sm:p-2">#{props.tag[1]}</span>
+    return <span className="h-min text-nowrap bg-nostr text-white rounded-xl px-2 py-1 sm:p-2">#{props.tag[1]}</span>
 }
 
-// TODO: Maybe put below images on large screens
 export default function ({ tags }: { tags: NDKTag[] }) {
     if (!tags.length) return null
 
     return (
-        <div className="product-tags flex gap-2 flex-nowrap no-scrollbar no-wrap overflow-x-scroll">
+        <div className="product-tags flex items-center gap-2 flex-nowrap no-scrollbar no-wrap overflow-x-scroll">
             {tags
                 .filter(tag => tag[0] === "t")
                 .map((tag, i) => (
