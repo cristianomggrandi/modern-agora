629b2f0ee091a5e5310c4eb6711f7f04d1d0adb8
Cristiano Grandi <80713582+cristianomggrandi@users.noreply.github.com>
2025-05-18 17:44:10 -0300

    Try to avoid error

--- src/hooks/useNDKStore.tsx
+++ src/hooks/useNDKStore.tsx
@@ -10,16 +10,7 @@ import {
     NDKParsedProductEvent,
     NDKParsedStallEvent,
 } from "@/utils/ndk"
-import NDK, {
-    NDKEvent,
-    NDKFilter,
-    NDKKind,
-    NDKNip07Signer,
-    NDKSubscription,
-    NDKSubscriptionOptions,
-    NDKTag,
-    NDKUser,
-} from "@nostr-dev-kit/ndk"
+import NDK, { NDKEvent, NDKFilter, NDKKind, NDKSubscription, NDKSubscriptionOptions, NDKTag, NDKUser } from "@nostr-dev-kit/ndk"
 import { create } from "zustand"
 
 const defaultRelays = [
@@ -80,16 +71,19 @@ type NDKStoreType = {
     unSubscribeToMessages: () => void
 }
 
-const ndk = window
-    ? window.nostr
-        ? new NDK({
-              explicitRelayUrls: defaultRelays,
-              signer: new NDKNip07Signer(),
-          })
-        : new NDK({
-              explicitRelayUrls: defaultRelays,
-          })
-    : undefined
+// const ndk = window
+//     ? window.nostr
+//         ? new NDK({
+//               explicitRelayUrls: defaultRelays,
+//               signer: new NDKNip07Signer(),
+//           })
+//         : new NDK({
+//               explicitRelayUrls: defaultRelays,
+//           })
+//     : undefined
+const ndk = new NDK({
+    explicitRelayUrls: defaultRelays,
+})
 
 ndk?.connect().catch(error => console.error("ndk error connecting", error))
 
